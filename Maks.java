4bfb11f7c832fbe2bff10d6469200086e0fac93c
Giofanny Mowoka <giofanny.mowoka@gmail.com>
2017-01-31 21:08:44 +0700

    mencari nilai max

---
+++ Maks.java
@@ -0,0 +1,19 @@
+class maks {
+public static void main(String []args){
+    int a= 30;
+    int b= 70;
+    int c= 60;
+    
+    int maks = 0;
+    
+    if(a > maks)
+        maks = a;
+    if(b > maks)
+        maks = b;
+    if(c > maks)
+        maks = c;
+    
+System.out.println("nilai Maks dari angka 30,70,60 adalah "+maks);
+    
+}
+}
